41c33598fe2ef22acb3852a18d9eed4a4f3fd62e
Mazin Fouad <m.fouad@live.de>
2023-02-14 15:36:52 +0100

    media queries for landingpage fixed

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AboutMeComponent } from './about-me/about-me.component';
 import { LandingPageComponent } from './landing-page/landing-page.component';
 
-const routes: Routes = [{ path: '', component: LandingPageComponent }];
+const routes: Routes = [
+  { path: '', component: LandingPageComponent },
+  { path: '', component: AboutMeComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
