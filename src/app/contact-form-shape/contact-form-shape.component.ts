4906cd9861e0c767791144f2c48724cfa7cc3877
Mazin Fouad <m.fouad@live.de>
2023-02-19 19:35:26 +0100

    added gitub links to portfolio section

--- src/app/contact-form-shape/contact-form-shape.component.ts
+++ src/app/contact-form-shape/contact-form-shape.component.ts
@@ -1,10 +1,13 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import * as AOS from 'aos';
 
 @Component({
   selector: 'app-contact-form-shape',
   templateUrl: './contact-form-shape.component.html',
-  styleUrls: ['./contact-form-shape.component.scss']
+  styleUrls: ['./contact-form-shape.component.scss'],
 })
-export class ContactFormShapeComponent {
-
+export class ContactFormShapeComponent implements OnInit {
+  ngOnInit() {
+    AOS.init();
+  }
 }
--- src/app/contact/contact.component.ts
+++ src/app/contact/contact.component.ts
@@ -1,11 +1,16 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import * as AOS from 'aos';
 
 @Component({
   selector: 'app-contact',
   templateUrl: './contact.component.html',
   styleUrls: ['./contact.component.scss'],
 })
-export class ContactComponent {
+export class ContactComponent implements OnInit {
+  ngOnInit() {
+    AOS.init();
+  }
+
   scrollToTop(): void {
     window.scrollTo({ top: 0, behavior: 'smooth' });
   }
